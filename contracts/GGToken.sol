01bc58d268950fc762a098fbe193b464ab88f349
Jordan Rowley <jordanrowley@Jordans-MacBook-Pro.local>
2020-08-15 20:43:53 +0100

    3. Delegated Transfer

--- contracts/GGToken.sol
+++ contracts/GGToken.sol
@@ -19,7 +19,17 @@ contract GGToken {
 		uint256 _value
 	);
 
+	//Approve Event
+	event Approval(
+		address indexed _owner,
+		address indexed _spender,
+		uint256 _value
+	);
+
+	//Account Balances
 	mapping(address => uint256) public balanceOf;
+	//Allowance
+	mapping(address => mapping(address => uint256)) public allowance;
 
 	//Constructor
 	constructor(uint256 _initialSupply) public {
@@ -37,6 +47,35 @@ contract GGToken {
 		//Emit Transfer Event
 		emit Transfer(msg.sender, _to, _value);
 		//Return a boolean
+		return true;
+	}
+
+	//Delegated Transfer
+
+	//Approve
+	function approve(address _spender, uint256 _value) public returns (bool success) {
+		//Allowance
+		allowance[msg.sender][_spender] = _value;
+		//Approval event
+		emit Approval(msg.sender, _spender, _value);
+		//Return a boolean
+		return true;
+	}
+
+	//TransferFrom
+	function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+		//Require _from has enough tokens
+		require(_value <= balanceOf[_from]);
+		//Require allowance is big enough
+		require(_value <= allowance[_from][msg.sender]);
+		//Change the balance
+		balanceOf[_from] -= _value;
+		balanceOf[_to] += _value;
+		//Update the allowance
+		allowance[_from][msg.sender] -= _value;
+		//Transfer Event
+		emit Transfer(_from, _to, _value);
+		//Returns a boolean
 		return true;
 	}
 
